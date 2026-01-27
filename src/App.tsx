3cea21c352d35b5f4505ca764640f70fc5c61177
gachip1234 <nguyenvanhung0101@gmail.com>
2026-01-27 19:01:57 +0700

    them git

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+      <div className="bg-white p-8 rounded-2xl shadow-xl text-center max-w-md border border-slate-200">
+        <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-linear-to-r from-blue-600 to-cyan-500 mb-4">
+          WELCOME REACT
+        </h1>
+        <p className="text-slate-600 text-lg mb-6">
+          Đây là dự án mẫu React + TypeScript + TailwindCSS v4. Đã cài đặt sẵn,
+          chỉ việc code!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
